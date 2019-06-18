11c03310d260752e829bfcd9e344bf6dfa9100f6
obaidullah obi <obaidullahahmad3965@gmail.com>
2019-06-18 13:09:26 +0200

    Contact form (Note: Der var 2 projektmapper inden i hinanden)

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,23 +1,31 @@
 const mysql = require('../config/mysql'); // mysql
-
+async function getCategories() {
+   let db = await mysql.connect();
+   let [categories] = await db.execute(`
+      SELECT category_id, category_title 
+      FROM categories
+      ORDER BY category_title ASC`);
+   db.end();
+   return categories;
+}
 module.exports = (app) => {
 
-   // Test
-   app.get('/database', async(req,res,next)=>{
+   //    // Test
+   app.get('/database', async (req, res, next) => {
       let db = await mysql.connect();
       let [newspage] = await db.execute('SELECT * FROM articles');
       db.end();
       // res.send(products)
       // inde i routen
       res.render('newspage', {
-       'newspage': newspage
-});
-   })     
-   
+         'newspage': newspage
+      });
+   })
+
    // Hvad skal der ske på denne side?
    // (...)
 
-   app.get('/', async function(req, res, next){
+   app.get('/', async function (req, res, next) {
 
       // featurePostArea = [{
       //    image: "img/bg-img/19.jpg",
@@ -30,72 +38,72 @@ module.exports = (app) => {
       let [categories] = await db.execute('SELECT * FROM categories');
       db.end();
 
-      res.render('home.ejs',{
-         "title":"The News Paper - News &amp; Lifestyle Magazine Template",
-         "categories":categories
-      }) 
-   
-      
-      
+      res.render('home.ejs', {
+         "title": "The News Paper - News &amp; Lifestyle Magazine Template",
+         "categories": categories
+      })
+
+
+
    })
 
    // Hvad skal der ske på denne side?
    // (...)
-   
-   app.get('/about', async function(req,res, next){
+
+   app.get('/about', async function (req, res, next) {
       let db = await mysql.connect();
       let [categories] = await db.execute('SELECT * FROM categories');
       db.end();
-      res.render('about.ejs',{
-         "title":"The News Paper - News &amp; Lifestyle Magazine Template",
-         "categories":categories
+      res.render('about.ejs', {
+         "title": "The News Paper - News &amp; Lifestyle Magazine Template",
+         "categories": categories
       })
    })
 
 
-   
-   
+
+
    // Hvad skal der ske på denne side?
    // (...)
 
    // Overvej om jeg skal omdøbe denne route til: "articlesByCategory"
 
-   app.get('/categories',async function(req,res, next){
+   app.get('/categories', async function (req, res, next) {
       let db = await mysql.connect();
       let [categories] = await db.execute('SELECT * FROM categories');
       db.end();
-       
+
       let commentsList = [{
          name: "JAMES SMITH",
-         text:"Facebook is offering facial recognition..",
-         image:"img/bg-img/29.jpg",
+         text: "Facebook is offering facial recognition..",
+         image: "img/bg-img/29.jpg",
          date: "06:34 am, April 14, 2018"
       },
       {
          name: "ANNA HANSEN",
-         text:"Facebook is offering facial recognition..",
-         image:"img/bg-img/30.jpg",
+         text: "Facebook is offering facial recognition..",
+         image: "img/bg-img/30.jpg",
          date: "06:34 am, April 15, 2018"
       },
-      
+
       {
          name: "CHRISTINA PRIOR",
-         text:"Facebook is offering facial recognition..",
-         image:"img/bg-img/31.jpg",
+         text: "Facebook is offering facial recognition..",
+         image: "img/bg-img/31.jpg",
          date: "06:34 am, April 15, 2018"
       }
-   ]
-      res.render('categories.ejs',{
-         "title":"The News Paper - News &amp; Lifestyle Magazine Template",
-         "categories":categories,
+      ]
+      res.render('categories.ejs', {
+         "title": "The News Paper - News &amp; Lifestyle Magazine Template",
+         "categories": categories,
          "latestCommentsList": commentsList
       })
 
-      
+
    })
 
-   app.get('/categories/:category_id',async function(req,res, next){
-      
+   app.get('/categories/:category_id', async function (req, res, next) {
+
       let db = await mysql.connect();
 
       // Til Navigation
@@ -113,71 +121,137 @@ module.exports = (app) => {
          `,
          [req.params.category_id]
       );
-       
-      
+
+
       db.end();
       let commentsList = [{
          name: "JAMES SMITH",
-         text:"Facebook is offering facial recognition..",
-         image:"img/bg-img/29.jpg",
+         text: "Facebook is offering facial recognition..",
+         image: "img/bg-img/29.jpg",
          date: "06:34 am, April 14, 2018"
       },
       {
          name: "ANNA HANSEN",
-         text:"Facebook is offering facial recognition..",
-         image:"img/bg-img/30.jpg",
+         text: "Facebook is offering facial recognition..",
+         image: "img/bg-img/30.jpg",
          date: "06:34 am, April 15, 2018"
       },
-      
+
       {
          name: "CHRISTINA PRIOR",
-         text:"Facebook is offering facial recognition..",
-         image:"img/bg-img/31.jpg",
+         text: "Facebook is offering facial recognition..",
+         image: "img/bg-img/31.jpg",
          date: "06:34 am, April 15, 2018"
       }
-   ]
-      res.render('categories.ejs',{
-         "title":"The News Paper - News &amp; Lifestyle Magazine Template",
-         "categories":categories,  // Nav
+      ]
+      res.render('categories.ejs', {
+         "title": "The News Paper - News &amp; Lifestyle Magazine Template",
+         "categories": categories,  // Nav
          "articlesByCategory": articlesByCategory,
-         
+
          "latestCommentsList": commentsList
       })
 
-      
+
    })
 
-   
-   app.get('/single-post', async function(req, res, next){
+
+   app.get('/single-post', async function (req, res, next) {
       // DB: hent alle kategorier
       let db = await mysql.connect();
       let [categories] = await db.execute('SELECT * FROM categories');
       db.end();
       console.log(categories)
 
-      res.render('single-post.ejs',{
-         "title":"The News Paper - News &amp; Lifestyle Magazine Template",
-         "categories":categories
+      res.render('single-post.ejs', {
+         "title": "The News Paper - News &amp; Lifestyle Magazine Template",
+         "categories": categories
       })
    })
-   
-   
-   app.get('/contact',async function(req, res, next){
+
+
+   app.get('/contact', async function (req, res, next) {
 
       let db = await mysql.connect();
       let [categories] = await db.execute('SELECT * FROM categories');
+
       db.end();
-      res.render('contact.ejs',{
-         "title":"The News Paper - News &amp; Lifestyle Magazine Template",
-         "categories":categories,
+      res.render('contact.ejs', {
+         "title": "The News Paper - News &amp; Lifestyle Magazine Template",
+         "categories": categories,
+
       })
    })
 
-       
-   // app.get('/', (req, res, next) => {
-   //    let now = new Date('2019-01-14 07:00:14');
-   //    console.log(app.locals.dateAndTime.format(now, 'h:mm A | MMMM DD'));
-   // });
+   app.post('/contact', async (req, res, next) => {
+
+      let name = req.body.name;
+      let email = req.body.email;
+      let subject = req.body.subject;
+      let message = req.body.message;
+      let contactDate = new Date();
+
+      //here we should create an empty array to handle our all input messages
+      let return_message = [];
+      //for validation, it will push our all values into our empty array
+      if (name == 'undefined' || name == '') {
+         return_message.push('Please Enter Your Name')
+      }
+      if (email == 'undefined' || email == '') {
+         return_message.push('Please Enter Your Email');
+      }
+      if (subject == 'undefined' || subject == '') {
+         return_message.push('Please Enter Your Subject');
+      }
+      if (message == 'undefined' || message == '') {
+         return_message.push('Please Enter Your Message')
+      }
+      if (return_message.length > 0) {
+         let categories = await getCategories(); // denne forklares lige om lidt!
+         res.render('contact', {
+            'categories': categories,
+            'return_message': return_message.join(', '),
+            'values': req.body // læg mærke til vi "bare" sender req.body tilbage
+         });
+
+      } else {
+         let db = await mysql.connect();
+
+         try{
+         let result = await db.execute(`
+         INSERT INTO messages 
+         (message_name, message_email, message_subject, message_text, message_date) 
+         VALUES 
+         (?,?,?,?,?)`, [name, email, subject, message, contactDate]);
+         db.end();
+         // send det modtagede data tilbage, så vi kan se det er korrekt
+         // res.send(req.body);
+         // affected rows er større end nul, hvis en (eller flere) række(r) blev indsat
+         if (result[0].affectedRows > 0) {
+            return_message.push('Tak for din besked, vi vender tilbage hurtigst muligt');
+         } else {
+            return_message.push('Din besked blev ikke modtaget.... ');
+         }
+      }catch(errors){
+            
+         };
+
+         let categories = await getCategories(); // denne har jeg ikke forklaret endnu! 
+         res.render('contact', {
+            
+            'categories': categories,
+            'return_message': return_message.join(', '),
+            //  'values': []
+         });
+      }
+
+      res.render('req.body')
+   })
+
+   app.get('/', (req, res, next) => {
+      let now = new Date('2019-01-14 07:00:14');
+      console.log(app.locals.dateAndTime.format(now, 'h:mm A | MMMM DD'));
+   });
 
 };
 
@@ -189,7 +263,7 @@ SELECT
 	articles.article_title,
     articles.article_postdate,
     articles.article_image,
-    
+
     categories.category_id,
     categories.category_title
 FROM `articles`
